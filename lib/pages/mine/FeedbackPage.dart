1ed225fd32816b3edde2debbd7d642de5735967e
mashneh <82058654+mashneh@users.noreply.github.com>
2021-08-10 18:44:42 +0100

    Update FeedbackPage.dart

--- lib/pages/mine/FeedbackPage.dart
+++ lib/pages/mine/FeedbackPage.dart
@@ -1,33 +1,256 @@
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class FeedbackPage extends StatefulWidget {
+class Feedback extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-
-    return new Page();
-  }
+  _FeedbackState createState() => _FeedbackState();
 }
 
-class Page extends State<FeedbackPage> {
-  @override
-  Widget build(BuildContext context) {
-    return layout(context);
+class _FeedbackState extends State<Feedback> {
+  launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
-  Widget layout(BuildContext context) {
-    /*24 is for notification bar on Android*/
-    return new Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 104, 47, 157),
-          shadowColor: Colors.white,
-          title: Text(
-            'Feedback',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        body: Center(
-          child: Text("Coming Soon"),
-        ));
+  _launchURLMail() async {
+    const url =
+        'mailto: mashneh9@gmail.com?subject=LifePlusApp&body=Your sugestions%20or ReachUs..';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
+
+  TextEditingController t1 = TextEditingController();
+  TextEditingController t2 = TextEditingController();
+  String name;
+  String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        title: Text("Feedback"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 14.0, horizontal: 13),
+              child: Text(
+                "Send us a message, and we'll get in touch with you as soon as possible. ",
+                style: TextStyle(
+                  fontSize: 17.5,
+                  height: 1.3,
+                  fontFamily: 'RobotoSlab',
+                ),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                onChanged: (val) {
+                  if (val != null || val.length > 0) name = val;
+                },
+                controller: t1,
+                decoration: InputDecoration(
+                  fillColor: Color(0xffe6e6e6),
+                  filled: true,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  hintText: 'Your name',
+                  hintStyle: TextStyle(
+                      color: Colors.blueGrey, fontFamily: 'RobotoSlab'),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide: BorderSide(color: Colors.grey[400]),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide: BorderSide(color: Colors.grey[400]),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide: BorderSide(color: Colors.grey[400]),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.0001,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                onChanged: (val) {
+                  if (val != null || val.length > 0) message = val;
+                },
+                textAlign: TextAlign.start,
+                controller: t2,
+                decoration: InputDecoration(
+                  fillColor: Color(0xffe6e6e6),
+                  filled: true,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 35, horizontal: 20),
+                  hintText: 'Your message',
+                  hintStyle: TextStyle(
+                    color: Colors.blueGrey,
+                    fontFamily: 'RobotoSlab',
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(17),
+                    ),
+                    borderSide: BorderSide(color: Colors.grey[400]),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(17),
+                    ),
+                    borderSide: BorderSide(color: Colors.grey[400]),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(17),
+                    ),
+                    borderSide: BorderSide(color: Colors.grey[400]),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Card(
+              color: Colors.green[300],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 10.0),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    t1.clear();
+                    t2.clear();
+                    launchUrl(
+                        "mailto:mashneh9@gmail.com?subject=From $name&body=$message");
+                  });
+                },
+                child: ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                          child: Icon(
+                        Icons.send,
+                        color: Colors.white,
+                      )),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.03,
+                      ),
+                      Center(
+                          child: Text(
+                        "Send",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white, fontFamily: 'RobotoSlab'),
+                      )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.06,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.05,
+                  left: 21,
+                  right: 21,
+                  bottom: MediaQuery.of(context).size.height * 0.034),
+              child: Text(
+                "Alternatively, you can also report bugs and errors on following platforms",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'RobotoSlab',
+                  color: Colors.blueGrey[600],
+                  fontSize: 17,
+                  height: 1.3,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () => launchUrl(
+                      "https://github.com/mashneh"),
+                  child: Icon(
+                    FontAwesomeIcons.github,
+                    color: Colors.orange,
+                    size: 35,
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.06,
+                ),
+                GestureDetector(
+                  onTap: () => launchUrl(
+                      "https://play.google.com/store/apps/details?id=com.lifeplusapp&hl=en_IN"),
+                  child: Icon(FontAwesomeIcons.googlePlay,
+                      color: Color(0xfffb3958), size: 35),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.06,
+                ),
+                GestureDetector(
+                  onTap: () => _launchURLMail(),
+                  child: Icon(FontAwesomeIcons.at,
+                      color: Color(0xff1DA1F2), size: 35),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
 }
